a3e0c3b1f9a5ebfc5976ff5646cfe98fac06008e
Clinton26 <134915423+Wanyangu1@users.noreply.github.com>
2025-08-24 19:12:04 +0300

    wip: latest changes

--- src/components/sections/Hero.tsx
+++ src/components/sections/Hero.tsx
@@ -246,7 +246,7 @@ const Hero = () => {
       className="group backdrop-blur-sm bg-white/10 border-white/20 text-white hover:bg-white/20"
     >
       <Play className="w-5 h-5 mr-2 group-hover:scale-110 transition-transform" />
-      Watch Our Story
+      Impact Stories
     </Button>
   </a>
 </div>
